b2d60140c2fa6293100897e6e0afc04804317ea8
Cao21745 <57550910+IceWolf777@users.noreply.github.com>
2021-04-03 19:08:23 -0700

    Update source.py
    v0.1

--- source.py
+++ source.py
@@ -3,25 +3,19 @@ from discord.ext import commands
 
 
 discordPublicId = "9198975fb26be78918e25d31e4efdbf1e6f7d4defc83a9d2de191fbe92bd4f52"
-bot = commands.Bot(command_prefix='>')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+client = discord.Client()
 
-bot.run('token')
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
-client = MyClient()
-client.run('token')
+client.run(discordPublicId)
